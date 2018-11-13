31659d56e92ef69b9a8bd82ea37d87432b5f81ac
mustachebutter <duylk99@gmail.com>
2018-11-12 21:43:07 -0500

    Commit 7

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
+
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
@@ -23,7 +25,11 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
+private:
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 90.f;
 		
-	
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume *PressurePlate;
+
 };
