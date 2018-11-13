b24c94ea7816fce1c39bece69f1d0712fb6aaca1
mustachebutter <duylk99@gmail.com>
2018-11-12 21:52:58 -0500

    Commit 8 (Temp)

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -20,6 +20,11 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
+	OpenDoor();
+}
+
+void UOpenDoor::OpenDoor()
+{
 	// Find the owning actor
 	AActor *Owner = GetOwner();
 	//Create Rotator
@@ -34,6 +39,11 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Poll the Trigger Volume 
+	// If the ActorThatOpens is in the volume 
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		OpenDoor();
+	}
 }
 
