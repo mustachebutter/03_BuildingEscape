4c31008a5d5341b72b99910f38b021dee4b10c60
mustachebutter <43947255+mustachebutter@users.noreply.github.com>
2018-11-15 18:03:36 -0500

    Commit 13

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -5,7 +5,7 @@
 #include "GameFramework/Actor.h"
 #include "GameFramework/PlayerController.h"
 #include "Classes/Engine/World.h"
-
+#include "Public/DrawDebugHelpers.h"
 #define OUT
 
 // Sets default values for this component's properties
@@ -46,6 +46,19 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	UE_LOG(LogTemp, Warning, TEXT("Location: %s Rotation: %s"), 
 		*PlayerViewPointLocation.ToString(), 
 		*PlayerViewPointRotation.ToString()
+	);
+	//Calculate LineTraceEnd
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector()  * Reach;
+	//Draw a red trace in the world to visualize 
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0.f,
+		10.f
 	);
 	//Ray-casting out to reach distance
 	
